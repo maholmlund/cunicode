c7fcb69e39f9f357bf6318a5069c95049c4bfd09
Miika Holmlund <miika.holmlund@gmail.com>
2024-10-25 19:14:03 +0300

    use unsigned char in Utf8CodePoint

--- src/cunicode.h
+++ src/cunicode.h
@@ -2,10 +2,9 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-struct Utf8CodePoint
-{
-        size_t len;
-        char bytes[4];
+struct Utf8CodePoint {
+	size_t len;
+	unsigned char bytes[4];
 };
 
 struct Utf8CodePoint Utf8CodePoint_from_bytes(unsigned char *bytes, size_t len);
